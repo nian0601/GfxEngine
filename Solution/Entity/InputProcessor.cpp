06e9dbecceeea4b968aecb21cf7c107343058816
nian0601 <anderssonniklas@outlook.com>
2016-07-08 15:01:54 +0200

    reduced movementspeed (hardcoded in InputProcessor)

--- Solution/Entity/InputProcessor.cpp
+++ Solution/Entity/InputProcessor.cpp
@@ -20,37 +20,36 @@ void InputProcessor::Update(float aDelta)
 {
 	CU::InputWrapper* inputWrapper = CU::InputWrapper::GetInstance();
 
+	float speed = 10.f;
 	CU::GrowingArray<Entity> entities = GetEntities();
 	for each (const Entity& entity in entities)
 	{
 		TranslationComponent& translation = GetComponent<TranslationComponent>(entity);
 
 		CU::Vector3<float> pos = translation.myOrientation.GetPos();
-
 		if (inputWrapper->KeyIsPressed(DIK_W))
 		{
-			pos.z += 100.f * aDelta;
+			pos.z += speed * aDelta;
 		}
 		else if (inputWrapper->KeyIsPressed(DIK_S))
 		{
-			pos.z -= 100.f * aDelta;
+			pos.z -= speed * aDelta;
 		}
 		else if (inputWrapper->KeyIsPressed(DIK_A))
 		{
-			pos.x -= 100.f * aDelta;
+			pos.x -= speed * aDelta;
 		}
 		else if (inputWrapper->KeyIsPressed(DIK_D))
 		{
-			pos.x += 100.f * aDelta;
+			pos.x += speed * aDelta;
 		}
 		else if (inputWrapper->KeyIsPressed(DIK_Q))
 		{
-			pos.y -= 100.f * aDelta;
-
+			pos.y -= speed * aDelta;
 		}
 		else if (inputWrapper->KeyIsPressed(DIK_E))
 		{
-			pos.y += 100.f * aDelta;
+			pos.y += speed * aDelta;
 		}
 
 		translation.myOrientation.SetPos(pos);
